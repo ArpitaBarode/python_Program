57164f929aca09443fbc2bc2df480c8fe4b7df5c
ArpitaBarode <100395290+ArpitaBarode@users.noreply.github.com>
2022-05-07 22:23:03 +0530

    Add files via upload

---
+++ print_numbers.py
@@ -0,0 +1,4 @@
+print("program for print numbers from 1 to 10")
+n=10
+for i in range(1,n+1):
+     print(i)
---
+++ print_table.py
@@ -0,0 +1,14 @@
+
+#write a python scriptt or printing table of user defined integer
+
+a=int(input("enter a value:"))
+m=a*10
+for i in range(a,m+1,a):
+       print(a,"*",i//a,"=",i)
+#method 2
+a=int(input("please enter value"))
+m=n*10
+count=1
+for i in range(n,n+1,n):
+    print("{}*{}={}".format(n,count,i))
+    count=count+1
