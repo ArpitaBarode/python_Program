e951542371969b45a7c487b1bf808a9c8b215371
ArpitaBarode <100395290+ArpitaBarode@users.noreply.github.com>
2022-05-11 00:05:31 +0530

    Add files via upload

---
+++ sort_string.py
@@ -0,0 +1,8 @@
+print("prohram to sort the string")
+my_str = input("Enter a string: ")    
+words = my_str.split()  
+# sort the list  
+words.sort()  
+# display the sorted words  
+for word in words:  
+   print(word) 
