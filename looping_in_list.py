f38fcbb4bb636fce8718c86d7aa5d5d67e372b3b
ArpitaBarode <100395290+ArpitaBarode@users.noreply.github.com>
2022-05-10 23:15:30 +0530

    Add files via upload

---
+++ looping_in_list.py
@@ -0,0 +1,8 @@
+print(10>9)
+help("keywords")
+plates = [1,2,3,4,5]
+for x in plates: 
+     print('I put a cake slice on plate number', x) 
+     print('All 5 plates are ready to be served!') 
+
+    
---
+++ set.py
@@ -0,0 +1,16 @@
+#set
+#it stores hitrozenous value
+#unorderd
+#un-indexed
+#duplicate value is not allow
+s={7,8,9,5}
+print(s)
+print(type(s))
+
+s=set()
+s.add(1)
+s.add(2)
+s.add(6)
+print(s)
+
+
